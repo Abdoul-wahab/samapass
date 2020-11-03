5fcf5667364af42c0fdbd65b0538b3f65982d9db
awz <awz@webreathe.fr>
2020-11-03 22:18:47 +0100

    initial commit

---
+++ database/migrations/2020_11_03_183153_create_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('city');
+            $table->string('category');
+            $table->text('description');
+            $table->date('start_at');
+            $table->date('start_day_at');
+            $table->date('start_hour_at');
+            $table->date('end_day_at');
+            $table->date('end_hour_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
